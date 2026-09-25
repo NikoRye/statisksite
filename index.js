c3c0cbd3fa0cfdf82153af2ae806becb75dc29c0
NikoRye <nibo1001@stud.ek.dk>
2026-09-25 08:30:51 +0200

    Fix formatting issues in index.html and enhance data fetching in index.js

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 "use strict";
+
 const productUrl = "https://kea-alt-del.dk/t7/api/categories";
+const categoryList = document.querySelector(".category_list_container");
+
 getData();
+
 function getData() {
-  fetch(productUrl).then((result) => result.json().then((data) => showData(data)));
+  fetch(productUrl)
+    .then((result) => result.json())
+    .then((data) => showData(data));
 }
 
 function showData(data) {
+  let myInnerHTML = "";
+
   data.forEach((element) => {
-    console.log("DATA, data");
+    myInnerHTML += `
+      <a href="produktliste.html">
+        <article class="category_card">
+          <h2>${element.category}</h2>
+        </article>
+      </a>
+    `;
   });
+
+  categoryList.innerHTML = myInnerHTML;
 }
